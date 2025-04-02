9282dd29e1749a926e4e959db6aa43a1da744567
Netetra <netetra07@gmail.com>
2025-04-02 19:35:59 +0900

    change: Dutyを削除してu16に変更

--- src/components/motor.rs
+++ src/components/motor.rs
@@ -2,20 +2,6 @@ use core::ops::Not;
 
 use num_enum::{FromPrimitive, IntoPrimitive};
 
-pub struct Duty(u16);
-
-impl From<u16> for Duty {
-    fn from(value: u16) -> Self {
-        Duty(value)
-    }
-}
-
-impl From<Duty> for u16 {
-    fn from(value: Duty) -> Self {
-        value.0
-    }
-}
-
 #[derive(IntoPrimitive, FromPrimitive, PartialEq)]
 #[repr(u8)]
 pub enum Dir {
@@ -36,9 +22,9 @@ impl Not for Dir {
 }
 
 pub trait Motor {
-    fn cw(&mut self, duty: impl Into<Duty>);
-    fn ccw(&mut self, duty: impl Into<Duty>);
-    fn run(&mut self, duty: impl Into<Duty>, dir: impl Into<Dir>) {
+    fn cw(&mut self, duty: u16);
+    fn ccw(&mut self, duty: u16);
+    fn run(&mut self, duty: u16, dir: impl Into<Dir>) {
         if dir.into() == Dir::Cw {
             self.cw(duty);
         } else {
