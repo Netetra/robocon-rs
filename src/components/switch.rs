3c163f597a80566b642f3575442e0353ca844f03
Netetra <netetra07@gmail.com>
2025-04-03 19:02:28 +0900

    feat: 一般的なスイッチの実装

--- src/components/switch.rs
+++ src/components/switch.rs
@@ -1,22 +1,44 @@
 use core::ops::Not;
 
+use embedded_hal::digital::InputPin;
 use num_enum::{FromPrimitive, IntoPrimitive};
 
 #[derive(IntoPrimitive, FromPrimitive, PartialEq)]
 #[repr(u8)]
-pub enum Switch {
+pub enum SwitchState {
     #[num_enum(default)]
-    Off = 0,
-    On = 1,
+    Open = 0,
+    Close = 1,
 }
 
-impl Not for Switch {
+impl Not for SwitchState {
     type Output = Self;
     fn not(self) -> Self::Output {
-        if self == Self::Off {
-            Self::On
+        if self == Self::Open {
+            Self::Close
         } else {
-            Self::Off
+            Self::Open
         }
+    }
+}
+
+pub struct Switch<P: InputPin> {
+    pin: P,
+    pull_up: bool,
+}
+
+impl<P: InputPin> Switch<P> {
+    pub fn new(pin: P, pull_up: bool) -> Self {
+        Self { pin, pull_up }
+    }
+    pub fn is_close(&mut self) -> bool {
+        if self.pull_up {
+            self.pin.is_low().unwrap()
+        } else {
+            self.pin.is_high().unwrap()
+        }
+    }
+    pub fn is_open(&mut self) -> bool {
+        !self.is_close()
     }
 }
