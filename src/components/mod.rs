520d258f3dc40376922729610ac08cd0c310d136
Netetra <netetra07@gmail.com>
2025-04-03 19:18:02 +0900

    feat: 一般的なロータリーエンコーダの定義

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,4 +1,5 @@
 pub mod gamepad;
 pub mod motor;
 pub mod omni;
+pub mod rotary_encoder;
 pub mod switch;
---
+++ src/components/rotary_encoder.rs
@@ -0,0 +1,26 @@
+use core::time::Duration;
+
+use super::motor::Dir;
+
+pub trait RotaryEncoder {
+    fn resolution(&self) -> u32;
+}
+
+pub trait Incremental: RotaryEncoder {
+    fn get_pulse(&self) -> u32;
+    fn get_dir(&self) -> Dir;
+    fn rotations(&self) -> f32 {
+        if self.get_dir() == Dir::Cw {
+            self.get_pulse() as f32 / self.resolution() as f32
+        } else {
+            -1. * self.get_pulse() as f32 / self.resolution() as f32
+        }
+    }
+    fn rpm(&self, dt: Duration) -> f32 {
+        self.rotations() / dt.as_secs_f32() * 60.
+    }
+}
+
+pub trait Absolute: RotaryEncoder {
+    fn get_position(&self) -> u32;
+}
