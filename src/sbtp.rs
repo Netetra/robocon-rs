ce00e4aafe2a551229bb5c29a5b4278501b1636c
Netetra <netetra07@gmail.com>
2025-04-04 20:44:53 +0900

    change: データ長をエスケープ後のペイロード長ではなくエスケープ前のペイロード長に変更

--- src/sbtp.rs
+++ src/sbtp.rs
@@ -4,11 +4,11 @@ use heapless::Vec;
 const GENERATE_POLYNOMIAL: u8 = 0xD5;
 const INITIAL_VALUE: u8 = 0xFF;
 
-const SBTP_START_BYTE: u8 = 0x55;
+const SBTP_SOF_BYTE: u8 = 0x55;
 const SBTP_ESCAPE_BYTE: u8 = 0x5A;
-const SBTP_END_BYTE: u8 = 0xAA;
+const SBTP_EOF_BYTE: u8 = 0xAA;
 const SBTP_XOR_BYTE: u8 = 0x42;
-const SBTP_FRAME_MAX_SIZE: usize = u8::MAX as usize + 4;
+const SBTP_FRAME_MAX_SIZE: usize = u8::MAX as usize * 2 + 4;
 const SBTP_PAYLOAD_MAX_SIZE: usize = u8::MAX as usize;
 
 fn crc8(data: &[u8]) -> u8 {
@@ -49,27 +49,19 @@ impl<IO: Read + Write> Sbtp<IO> {
             return Err(Error::PayloadOverflow);
         }
 
-        let mut payload_len = 0;
         for d in data {
-            if *d == SBTP_START_BYTE || *d == SBTP_END_BYTE || *d == SBTP_ESCAPE_BYTE {
-                if payload_len > (SBTP_PAYLOAD_MAX_SIZE - 2) {
-                    return Err(Error::PayloadOverflow);
-                }
+            if *d == SBTP_SOF_BYTE || *d == SBTP_EOF_BYTE || *d == SBTP_ESCAPE_BYTE {
                 buf.push(SBTP_ESCAPE_BYTE).unwrap();
                 buf.push(*d ^ SBTP_XOR_BYTE).unwrap();
-                payload_len += 2;
             } else {
-                if payload_len > (SBTP_PAYLOAD_MAX_SIZE - 1) {
-                    return Err(Error::PayloadOverflow);
-                }
                 buf.push(*d).unwrap();
-                payload_len += 1;
             }
         }
-        buf.insert(0, SBTP_START_BYTE).unwrap();
-        buf.insert(1, buf.len() as u8).unwrap();
+
+        buf.insert(0, SBTP_SOF_BYTE).unwrap();
+        buf.insert(1, data.len() as u8).unwrap();
         buf.push(crc8(data)).unwrap();
-        buf.push(SBTP_END_BYTE).unwrap();
+        buf.push(SBTP_EOF_BYTE).unwrap();
 
         if let Err(e) = self.transport.write_all(buf.as_slice()).await {
             return Err(Error::TransportError(e));
@@ -81,12 +73,13 @@ impl<IO: Read + Write> Sbtp<IO> {
         let mut payload = Vec::new();
 
         loop {
-            if self.read_byte().await? == SBTP_START_BYTE {
+            if self.read_byte().await? == SBTP_SOF_BYTE {
                 break;
             }
         }
 
         let len = self.read_byte().await?;
+
         for _ in 0..len {
             let byte = self.read_byte().await?;
             if byte == SBTP_ESCAPE_BYTE {
@@ -99,7 +92,7 @@ impl<IO: Read + Write> Sbtp<IO> {
         }
         let crc = self.read_byte().await?;
 
-        if self.read_byte().await? != SBTP_END_BYTE {
+        if self.read_byte().await? != SBTP_EOF_BYTE {
             return Err(Error::InvalidFormat);
         }
 
