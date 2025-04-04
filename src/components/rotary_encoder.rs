22ea87180313480af7d5ac856831673a270523d0
Netetra <netetra07@gmail.com>
2025-04-04 20:51:27 +0900

    feat: reset_countを追加

--- src/components/rotary_encoder.rs
+++ src/components/rotary_encoder.rs
@@ -7,13 +7,15 @@ pub trait RotaryEncoder {
 }
 
 pub trait Incremental: RotaryEncoder {
-    fn get_pulse(&self) -> u32;
+    fn get_count(&self) -> u32;
     fn get_dir(&self) -> Dir;
+    fn reset_count(&mut self);
+
     fn rotations(&self) -> f32 {
         if self.get_dir() == Dir::Cw {
-            self.get_pulse() as f32 / self.resolution() as f32
+            self.get_count() as f32 / self.resolution() as f32
         } else {
-            -1. * self.get_pulse() as f32 / self.resolution() as f32
+            -1. * self.get_count() as f32 / self.resolution() as f32
         }
     }
     fn rpm(&self, dt: Duration) -> f32 {
